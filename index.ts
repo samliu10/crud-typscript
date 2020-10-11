7a51d473dcc4247f303570fa9fa45299b17d2c38
Sam Liu <sl2486@cornell.edu>
2020-10-10 22:50:39 -0400

    Implemented CRUD functions
    - Get reviews, sort by location, get ratings, update a review, delete a
    review

--- index.ts
+++ index.ts
@@ -12,5 +12,96 @@ const db = admin.firestore();
 const app = express();
 const PORT = 8080;
 app.use(bodyParser.json());
+const reviewCollection = db.collection('reviews');
+
+type Review = {
+  name: string;
+  location: string;
+  stars: string;
+  comments: string;
+  date: Date;
+  id: string;
+}
+
+/** Returns an array of documents. */
+const createArr = (queries: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>) => {
+  const reviews: Review[] = [];
+  for (let doc of queries.docs) {
+    let review: Review = doc.data() as Review;
+    review.id = doc.id;
+    reviews.push(review);
+  }
+  return reviews;
+}
+
+/** Retrieve all reviews for a location.*/
+const getReviewsByLocation = async (location: string) => {
+  const locationReviews = await reviewCollection.where('location', '==', location).get();
+  const reviews: Review[] = createArr(locationReviews);
+  return reviews;
+}
+
+/** Get reviews sorted by [field] in [dir] order. */
+const getSortedReviews = async (field: string, dir: "desc" | "asc" | undefined) => {
+  const sortedReviews = await reviewCollection.orderBy(field, dir).get();
+  const reviews: Review[] = createArr(sortedReviews);
+  return reviews;
+}
+
+/** Write a review. */
+app.post('/reviews/newreview/', async (req, res) => {
+  const doc = reviewCollection.doc();
+  const review: Review = req.body;
+  doc.set(review);
+  res.status(201).send(doc.id);
+})
+
+/** Get all reviews in descending order by date. */
+app.get('/reviews', async (req, res) => {
+  const reviews: Review[] = await getSortedReviews('date', 'desc');
+  res.status(200).send(reviews);
+})
+
+/** Get all reviews for a location. */
+app.get('/reviews/bylocation/:location/', async (req, res) => {
+  const reviews: Review[] = await getReviewsByLocation(req.params.location);
+  res.status(200).send(reviews);
+})
+
+/** Get review by id. */
+app.get('/reviews/id/:id/', async (req, res) => {
+  const id = req.params.id;
+  const doc = await reviewCollection.doc(id).get();
+  const review: Review = doc.data() as Review;
+  review.id = doc.id;
+  res.status(200).send(review);
+})
+
+/** Get the average rating for a location. */
+app.get('/reviews/ratings/:location/', async (req, res) => {
+  const reviews: Review[] = await getReviewsByLocation(req.params.location);
+  let sum: number = 0;
+  for (let review of reviews) {
+    sum += parseInt(review.stars);
+  }
+  const avg: string = (sum / reviews.length).toString();
+  res.status(200).send(avg);
+})
+
+/** Edit a review. */
+app.post('/reviews/edit/:id/', async (req, res) => {
+  const id: string = req.params.id;
+  const newReview = req.body;
+  await reviewCollection.doc(id).update(newReview);
+  res.status(201).send(id);
+})
+
+/** Delete a review. */
+app.delete('/reviews/delete/:id/', async (req, res) => {
+  const id: string = req.params.id;
+  await reviewCollection.doc(id).delete();
+  res.status(200).send(id);
+})
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
--- output/index.js
+++ output/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,4 +24,75 @@ const db = firebase_admin_1.default.firestore();
 const app = express_1.default();
 const PORT = 8080;
 app.use(body_parser_1.default.json());
+const reviewCollection = db.collection('reviews');
+/** Returns an array of documents. */
+const createArr = (queries) => {
+    const reviews = [];
+    for (let doc of queries.docs) {
+        let review = doc.data();
+        review.id = doc.id;
+        reviews.push(review);
+    }
+    return reviews;
+};
+/** Retrieve all reviews for a location.*/
+const getReviewsByLocation = (location) => __awaiter(void 0, void 0, void 0, function* () {
+    const locationReviews = yield reviewCollection.where('location', '==', location).get();
+    const reviews = createArr(locationReviews);
+    return reviews;
+});
+/** Get reviews sorted by [field] in [dir] order. */
+const getSortedReviews = (field, dir) => __awaiter(void 0, void 0, void 0, function* () {
+    const sortedReviews = yield reviewCollection.orderBy(field, dir).get();
+    const reviews = createArr(sortedReviews);
+    return reviews;
+});
+/** Write a review. */
+app.post('/reviews/newreview/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const doc = reviewCollection.doc();
+    const review = req.body;
+    doc.set(review);
+    res.status(201).send(doc.id);
+}));
+/** Get all reviews in descending order by date. */
+app.get('/reviews', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const reviews = yield getSortedReviews('date', 'desc');
+    res.status(200).send(reviews);
+}));
+/** Get all reviews for a location. */
+app.get('/reviews/bylocation/:location/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const reviews = yield getReviewsByLocation(req.params.location);
+    res.status(200).send(reviews);
+}));
+/** Get review by id. */
+app.get('/reviews/id/:id/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const doc = yield reviewCollection.doc(id).get();
+    const review = doc.data();
+    review.id = doc.id;
+    res.status(200).send(review);
+}));
+/** Get the average rating for a location. */
+app.get('/reviews/ratings/:location/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const reviews = yield getReviewsByLocation(req.params.location);
+    let sum = 0;
+    for (let review of reviews) {
+        sum += parseInt(review.stars);
+    }
+    const avg = (sum / reviews.length).toString();
+    res.status(200).send(avg);
+}));
+/** Edit a review. */
+app.post('/reviews/edit/:id/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const newReview = req.body;
+    yield reviewCollection.doc(id).update(newReview);
+    res.status(201).send(id);
+}));
+/** Delete a review. */
+app.delete('/reviews/delete/:id/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    yield reviewCollection.doc(id).delete();
+    res.status(200).send(id);
+}));
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
